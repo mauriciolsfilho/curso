aa18bb1e16522d1133469ef00cc562c4022852b3
Mauricio Filho - TI <m.filho@kingspanisoeste.com.br>
2020-02-04 22:28:30 -0300

    iniciando curso

--- curso-api/src/main/java/com/curso/api/CursoApiApplication.java
+++ curso-api/src/main/java/com/curso/api/CursoApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoApiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApiApplication.class, args);
 	}
