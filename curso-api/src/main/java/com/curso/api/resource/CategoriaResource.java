f755005dd8e3659ca66e9ad56e1d7274e6017807
m.linhares <mauricio_1souza@outlook.com>
2020-02-21 00:41:17 -0300

    Comunicação da api

--- curso-api/src/main/java/com/curso/api/resource/CategoriaResource.java
+++ curso-api/src/main/java/com/curso/api/resource/CategoriaResource.java
@@ -1,10 +1,41 @@
 package com.curso.api.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.curso.api.model.Categoria;
+import com.curso.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public ResponseEntity<?> listar(){
+	    List<Categoria> categorias = categoriaRepository.findAll();
+        return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Categoria> findById(@PathVariable Long id){
+        return categoriaRepository.findById(id);
+
+    }
 }
